091b9bd7889b79859dc1a23ab91cfe2207a1d3bf
Edwin <edwinvrooij@outlook.com>
2017-01-08 13:30:34 +0100

    fixed some issues

--- app/src/main/java/me/evrooij/admin/MainActivity.java
+++ app/src/main/java/me/evrooij/admin/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 import butterknife.BindView;
@@ -32,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
         try {
             new Thread(() -> {
                 List<Feedback> feedbackList = feedbackManager.getAllFeedback();
@@ -41,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
                         refreshListView(feedbackList));
 
             }).start();
+
+            listView.setOnItemClickListener((adapter1, v, position, id) -> {
+                Feedback feedback = (Feedback) adapter1.getItemAtPosition(position);
+
+                ReturnBoolean r = result -> {
+                    if (result) {
+                        new Thread(() -> {
+                            ResponseMessage message = feedbackManager.deleteFeedback(feedback.getId());
+
+                            MainActivity.this.runOnUiThread(() -> {
+                                adapter.remove(feedback);
+
+                                Snackbar.make(listView, message.toString(), Snackbar.LENGTH_LONG).show();
+                            });
+                        }).start();
+                    }
+                };
+                new ConfirmDialog(MainActivity.this, r).show(MainActivity.this.getResources().getString(R.string.feedback_delete_warning));
+            });
         } catch (Exception e) {
             Log.e(TAG, "onCreate: ", e);
             Toast.makeText(this, String.format("Exception: %s", e.getMessage()), Toast.LENGTH_SHORT).show();
@@ -56,24 +75,6 @@ public class MainActivity extends AppCompatActivity {
         feedbackManager = new FeedbackManager();
 
 
-        listView.setOnItemClickListener((adapter1, v, position, id) -> {
-            Feedback feedback = (Feedback) adapter1.getItemAtPosition(position);
-
-            ReturnBoolean r = result -> {
-                if (result) {
-                    new Thread(() -> {
-                        ResponseMessage message = feedbackManager.deleteFeedback(feedback.getId());
-
-                        runOnUiThread(() -> {
-                            adapter.remove(feedback);
-
-                            Snackbar.make(listView, message.toString(), Snackbar.LENGTH_LONG).show();
-                        });
-                    }).start();
-                }
-            };
-            new ConfirmDialog(this, r).show(getResources().getString(R.string.feedback_delete_warning));
-        });
     }
 
     private void refreshListView(List<Feedback> feedbackList) {
